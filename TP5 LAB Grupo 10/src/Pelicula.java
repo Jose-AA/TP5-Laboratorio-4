b9a95c531c1ef302f3fa72e342e31cc1d8e2738f
Luciano Santostefano <luchoguit@gmail.com>
2024-09-25 19:27:12 -0300

    + Pelicula + Ventana Agregar

---
+++ TP5 LAB Grupo 10/src/Pelicula.java
@@ -0,0 +1,35 @@
+public class Pelicula {
+
+    private final int id;
+    private static int contadorId = 1;
+    private String nombre;
+    private Categoria categoria;
+
+    public Pelicula(String nombre, Categoria categoria) {
+        this.id = contadorId;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        contadorId++;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setnombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Pelicula: " + nombre + " - Categoría: " + categoria.getNombre();
+    }
+}
--- TP5 LAB Grupo 10/src/VentanaAgregar.java
+++ TP5 LAB Grupo 10/src/VentanaAgregar.java
@@ -1,12 +1,23 @@
-import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
 
 public class VentanaAgregar extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField txtID;
+	private JTextField txtNombre;
+	private JLabel lblID;
+	private JLabel lblNombre;
+	private JLabel lblGenero;
+	private JComboBox cbGenero;
+	private JButton btnAceptar;
 
 
 	public VentanaAgregar() {
@@ -14,9 +25,39 @@ public class VentanaAgregar extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		lblID = new JLabel("ID");
+		lblID.setBounds(97, 34, 12, 16);
+		contentPane.add(lblID);
+		
+		txtID = new JTextField();
+		txtID.setEditable(false);
+		txtID.setBounds(219, 31, 126, 22);
+		contentPane.add(txtID);
+		txtID.setColumns(10);
+		
+		lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(97, 75, 45, 16);
+		contentPane.add(lblNombre);
+		
+		txtNombre = new JTextField();
+		txtNombre.setBounds(219, 72, 126, 22);
+		contentPane.add(txtNombre);
+		txtNombre.setColumns(10);
+		
+		lblGenero = new JLabel("Genero");
+		lblGenero.setBounds(97, 117, 41, 16);
+		contentPane.add(lblGenero);
+		
+		cbGenero = new JComboBox();
+		cbGenero.setBounds(219, 114, 126, 22);
+		contentPane.add(cbGenero);
+		
+		btnAceptar = new JButton("ACEPTAR");
+		btnAceptar.setBounds(97, 164, 116, 25);
+		contentPane.add(btnAceptar);
 		setTitle("Agregar Pelicula");
 	}
-
 }

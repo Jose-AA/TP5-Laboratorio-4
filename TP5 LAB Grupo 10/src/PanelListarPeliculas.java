c8625bfc88bee32558f257b96109bce0735215da
Jose Arias <jose.arias@alumnos.frgp.utn.edu.ar>
2024-09-28 20:13:43 -0300

    Primera propuesta para mostrar las peliculas

--- TP5 LAB Grupo 10/src/PanelListarPeliculas.java
+++ TP5 LAB Grupo 10/src/PanelListarPeliculas.java
@@ -1,11 +1,36 @@
 import javax.swing.JPanel;
+import javax.swing.DefaultListModel;
+import javax.swing.JLabel;
+import javax.swing.JList;
+
 
 public class PanelListarPeliculas extends JPanel {
 
-	private static final long serialVersionUID = 1L;
-	
-	public PanelListarPeliculas() {
+    private static final long serialVersionUID = 1L;
+    private DefaultListModel<String> dlModel;
+    private JList<String> lista;
 
-	}
+    public PanelListarPeliculas() {
+        setLayout(null);
 
+        JLabel lblPeliculas = new JLabel("Películas");
+        lblPeliculas.setBounds(66, 109, 70, 20);
+        add(lblPeliculas);
+
+        lista = new JList<>();
+        lista.setBounds(183, 69, 212, 177);
+        add(lista);
+
+       
+        dlModel = new DefaultListModel<>();
+        cargarPeliculas();
+        lista.setModel(dlModel);
+    }
+    
+    public void cargarPeliculas() {
+        dlModel.clear(); 
+        for (Pelicula pelicula : Pelicula.listaPeliculas) {
+            dlModel.addElement(pelicula.toString());
+        }
+    }
 }
